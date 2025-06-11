fc90e3d2dcc50403ac5c97dd557607d70d541220
EzhikRuinit <no@thank.you>
2025-06-12 01:08:35 +0500

    Initial commit

---
+++ dwm/themes/mhtheme.h
@@ -0,0 +1,8 @@
+// mh's them
+// clang-format off
+
+static const char col_gray1[] = "#000000";
+static const char col_gray2[] = "#000000";
+static const char col_gray3[] = "#f3e7d5";
+static const char col_gray4[] = "#000000";
+static const char col_accnt[] = "#f1b45c";
